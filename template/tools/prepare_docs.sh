caabff10c89e440660a81c6bcf97fc55201a80ad
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-02-25 09:03:41 +0100

    Update branding and social icons in docs

--- template/tools/prepare_docs.sh
+++ template/tools/prepare_docs.sh
@@ -5,13 +5,13 @@ cp -R ../assets-docs/overrides/ overrides/
 
 echo "\033[0;33m:::::: Add the extra files from the ../assets-branding\033[0m"
 mkdir -p docs/assets/images/
-cp ../assets-branding/easydiffraction/hero/dark.png docs/assets/images/hero_dark.png
-cp ../assets-branding/easydiffraction/hero/light.png docs/assets/images/hero_light.png
-cp ../assets-branding/easydiffraction/logos/dark.svg docs/assets/images/logo_dark.svg
-cp ../assets-branding/easydiffraction/logos/light.svg docs/assets/images/logo_light.svg
-cp ../assets-branding/easydiffraction/icons/ed-icon_256x256.png docs/assets/images/favicon.png
+cp ../assets-branding/easypeasy/hero/dark.png docs/assets/images/hero_dark.png
+cp ../assets-branding/easypeasy/hero/light.png docs/assets/images/hero_light.png
+cp ../assets-branding/easypeasy/logos/dark.svg docs/assets/images/logo_dark.svg
+cp ../assets-branding/easypeasy/logos/light.svg docs/assets/images/logo_light.svg
+cp ../assets-branding/easypeasy/icons/color.png docs/assets/images/favicon.png
 mkdir -p overrides/.icons/
-cp ../assets-branding/easydiffraction/icons/ed-icon_bw.svg overrides/.icons/easydiffraction.svg
+cp ../assets-branding/easypeasy/icons/bw.svg overrides/.icons/easypeasy.svg
 cp ../assets-branding/easyscience-org/icons/eso-icon_bw.svg overrides/.icons/easyscience.svg
 
 echo "\033[0;33m:::::: Add Jupyter notebooks from the project to the docs/\033[0m"
